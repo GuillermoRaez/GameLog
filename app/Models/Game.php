4899627106d59cd896652e239af3e9b68430c05b
guillermo <guillaumeraez@gmail.com>
2021-07-16 06:02:16 +0200

    Model Game made.

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Game extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title', 'description'
+    ];
+
+    //A game can have an infinite number of parties
+    public function party (){
+        return $this -> hasMany(Party::class);
+    }
 }
