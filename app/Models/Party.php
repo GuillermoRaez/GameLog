a3bad906c0e7b2b9d59afc28896176bfb7d358b9
guillermo <guillaumeraez@gmail.com>
2021-07-18 15:00:50 +0200

    Added the relations properties to the party model.

--- app/Models/Party.php
+++ app/Models/Party.php
@@ -15,7 +15,21 @@ class Party extends Model
          'game_id'
      ];
     
-    
+
+    //A party can be in many user_parties (1:n).
+    public function Party_User (){
+        return $this -> HasMany(PartyUser::class);
+    }
+
+    //A party can only have one game (1:1).
+    public function game (){
+        return $this -> belongsTo(Game::class);
+    }
+
+    //A party can have many messages (1:n).
+    public function message (){
+        return $this -> HasMany(Message::class);
+    }
 
 }
 
